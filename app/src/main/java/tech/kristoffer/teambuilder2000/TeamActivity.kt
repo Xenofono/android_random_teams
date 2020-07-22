c54c68e600f7dc58e50363220fa615da6125fd33
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2020-07-22 14:23:33 +0200

    did some styling of the team member names

--- app/src/main/java/tech/kristoffer/teambuilder2000/TeamActivity.kt
+++ app/src/main/java/tech/kristoffer/teambuilder2000/TeamActivity.kt
@@ -29,7 +29,9 @@ class TeamActivity : AppCompatActivity() {
     }
 
     private fun createTeams() {
-        val shuffledPlayers = playerNames.filter { it.isNotBlank() }.shuffled()
+        val shuffledPlayers = playerNames.filter { it.isNotBlank() }
+            .map { it.trim() }
+            .shuffled()
         //om antalet lag är 1 under antalet spelare så tar vi en spelare i taget
         //tills vi har teamNumber-1, sen blir resterande sista laget
         //annars används inbyggda windowed
